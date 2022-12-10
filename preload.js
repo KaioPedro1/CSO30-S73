b0ac5123cf240542c71444892158e6b540b36fc6
KaioPedro1 <kaiopedro@alunos.utfpr.edu.br>
2022-12-10 11:51:00 -0300

    teste shell

--- preload.js
+++ preload.js
@@ -1,12 +1,19 @@
 const { contextBridge } = require('electron');
 const os = require('os')
 const si = require('systeminformation');
-
 contextBridge.exposeInMainWorld('OS', {
   getOsObj: () => os,
-  getSiObj: () => si
+  getSiObj: () => si,
+  getShell: () => shell
 });
 
 window.addEventListener('DOMContentLoaded', () => {
   document.getElementById("hardware").addEventListener("click", () => window.location.href = "./pages/detalhesSO.html");
+  document.getElementById("shell").addEventListener("click", openShell);
 })
+
+const openShell = () => {
+  const shell = require('shelljs');
+  shell.echo('Aberto pelo dashboard');
+  shell
+}
