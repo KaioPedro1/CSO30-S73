8ac7ea29ffea42cbed3bb83429e6dacc53a82e51
KaioPedro1 <kaiopedro@alunos.utfpr.edu.br>
2022-12-10 11:28:35 -0300

    dados estaticos

--- pages/detalhesSO.js
+++ pages/detalhesSO.js
@@ -1,11 +1,53 @@
 const os = OS.getOsObj();
 const siStaticData = OS.getSiObj();
 const osCpus = os.cpus();
-siStaticData.getStaticData().then(data=>gerarTabelaOS(data.os))
+
+getDataAndFillInfo();
+
+function getDataAndFillInfo() {
+    siStaticData.osInfo().then((osData) => gerarInfoGeneric(osData, 'os_tab', 'Dados do sistema operacional'));
+    siStaticData.baseboard().then((motherboardData) => gerarInfoGeneric(motherboardData, 'placa_mae_tab', 'Dados da placa mãe'));
+    siStaticData.bios().then((biosData) => gerarInfoGeneric(biosData, 'bios_tab', 'Dados da bios'));
+    siStaticData.diskLayout().then((diskLayoutData) => gerarInfoGeneric(diskLayoutData, 'disk_tab', 'Dados do disco'));
+    siStaticData.memLayout().then((memLayoutData) => gerarInfoGeneric(memLayoutData, 'mem_tab', 'Dados da memória ram'));
+    siStaticData.graphics().then((graphicsData) => gerarInfoGeneric(graphicsData.controllers, 'graphics_tab', 'Dados da placa de video'));
+}
+siStaticData.getStaticData().then(data => {
+    console.log(data)
+})
 window.addEventListener('DOMContentLoaded', () => {
     gerarTabelaCPU(document.getElementById('tabela_cpu_body'));
 })
+const gerarInfoGeneric = (data, containerID, titulo) => {
+    let osTabDiv = document.getElementById(containerID);
+    let listDiv = document.createElement('div');
+    while (osTabDiv.firstChild) {
+        osTabDiv.removeChild(osTabDiv.firstChild);
+    }
+    criarTitulo(osTabDiv, titulo)
+    if (!Array.isArray(data)) {
+        let arrOS = Object.entries(data);
+        arrOS.forEach((item) => {
+            let p = document.createElement('p');
+            p.innerHTML = `<strong>${item[0].toUpperCase()}:</strong> ${item[1]}`;
+            listDiv.appendChild(p)
+        })
+    } else {
+        data.forEach((obj,index) => {
+            let numero = document.createElement("h3");
+            numero.innerText = Number(index+1)+')';
+            osTabDiv.appendChild(numero)
+            let arrFromObj = Object.entries(obj);
+            arrFromObj.forEach((item) => {
+                let p = document.createElement('p');
+                p.innerHTML = `<strong>${item[0].toUpperCase()}:</strong> ${item[1]}`;
+                listDiv.appendChild(p)
+            })
+        })
+    }
 
+    osTabDiv.appendChild(listDiv);
+}
 const gerarTabelaCPU = (tabelaBody) => {
     osCpus.forEach(cpu => {
         let row = tabelaBody.insertRow(0);
@@ -25,20 +67,39 @@ const gerarTabelaCPU = (tabelaBody) => {
     document.getElementById('total_nucleos').innerHTML = `Total de núcleos: ${osCpus.length}`
     document.getElementById('cpu_arq').innerHTML = `${os.arch()}`
 }
-
+const criarTitulo = (containerHTML, text) => {
+    let titulo = document.createElement("h1");
+    titulo.className = "title"
+    titulo.innerText = text;
+    containerHTML.appendChild(titulo)
+}/*
 const gerarTabelaOS = (data) =>{ 
-    let osTabDiv = document.getElementById('os_tab');    
-    let osArray = Object.entries(data);
+    let osTabDiv = document.getElementById('os_tab');
+    let listDiv = document.createElement('div');
+    arrOS = Object.entries(data);   
     while (osTabDiv.firstChild) {
         osTabDiv.removeChild(osTabDiv.firstChild);
     }
     criarTitulo(osTabDiv, 'Dados do sistema operacional estático')
-   
-    console.log(osArray);
+    arrOS.forEach((item)=>{   
+        let p = document.createElement('p');
+        p.innerHTML = item[0].toUpperCase()+': '+item[1];
+        listDiv.appendChild(p)
+    })
+    osTabDiv.appendChild(listDiv);
 }
-const criarTitulo = (containerHTML, text) =>{
-    let titulo = document.createElement("h1");
-    titulo.className="title"
-    titulo.innerText=text;
-    containerHTML.appendChild(titulo)
-}
+const gerarTabelaMotherboard = (data) =>{
+    let osTabDiv = document.getElementById('placa_mae_tab');
+    let listDiv = document.createElement('div');
+    arrOS = Object.entries(data);   
+    while (osTabDiv.firstChild) {
+        osTabDiv.removeChild(osTabDiv.firstChild);
+    }
+    criarTitulo(osTabDiv, 'Dados do sistema operacional estático')
+    arrOS.forEach((item)=>{   
+        let p = document.createElement('p');
+        p.innerHTML = item[0].toUpperCase()+': '+item[1];
+        listDiv.appendChild(p)
+    })
+    osTabDiv.appendChild(listDiv);
+}*/
